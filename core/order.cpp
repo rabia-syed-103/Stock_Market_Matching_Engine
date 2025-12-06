ebb7b8304492695145a8baaaeaed189123a42a92
Ubuntu <ubuntu@ip-172-31-69-57.ec2.internal>
2025-12-06 15:06:18 +0000

    Order Class Done

---
+++ core/order.cpp
@@ -0,0 +1,49 @@
+#include "order.h"
+
+Order Order:: (int id,const string& uID,const string& sym,const string& sd,double prc, int qty)
+   : orderID(id), userID(uID), symbol(sym), side(sd),
+          price(prc), quantity(qty), remainingQty(qty), status("ACTIVE")
+    {
+        timestamp = std::time(nullptr);
+    }
+
+bool Order:: isFilled() const
+{
+    return remainingQty <= 0;
+
+};
+
+void  Order:: fill(int qty) 
+{
+    if (qty > remainingQty) 
+        qty = remainingQty;
+    remainingQty -= qty;
+    if (remainingQty == 0)
+         status = "FILLED";
+    else 
+        status = "PARTIAL_FILL";
+}
+
+void Order:: cancel()
+{
+     if (!isFilled()) {
+            status = "CANCELLED";
+            remainingQty = 0;
+        }
+}
+
+
+string Order:: toString() const {
+    
+    ostringstream oss;
+    oss << "OrderID: " << orderID
+        << ", User: " << userID
+        << ", Symbol: " << symbol
+        << ", Side: " << side
+        << ", Price: $" << std::fixed << std::setprecision(2) << price
+        << ", Qty: " << quantity
+        << ", Remaining: " << remainingQty
+        << ", Status: " << status
+        << ", Timestamp: " << timestamp;
+    return oss.str();
+}
